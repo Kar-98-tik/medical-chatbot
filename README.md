09f215336d5ee3990be7efb6b4272ddf5bd47675
Kartik <kartikprayagi0@gmail.com>
2024-11-16 19:45:46 +0530

    added folder structure

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # medical-chatbot
+
+conda create -n medbot python=3.8 -y
+
+conda activate medbot
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical ChatBot',
+    version = '0.0.1',
+    author = 'Kartik Prayagi',
+    author_email = 'kartikprayagi0@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
